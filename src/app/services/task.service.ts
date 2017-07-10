87a3fc7ff15c540fd0d091d13a790055586cc7fb
nan.dong <dongnan.fr@gmail.com>
2017-07-10 15:43:48 +0200

    read task info from json file

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -3,10 +3,16 @@ import {Task} from "../models/task";
 
 @Injectable()
 export class TaskService {
-    get(): Promise<Task[]>{
-        return Promise.resolve([
-            {id: 1, text: "Task #1", start_date: "2017-04-15 00:00", duration: 3, progress: 0.6},
-            {id: 2, text: "Task #2", start_date: "2017-04-18 00:00", duration: 3, progress: 0.4}
-        ]);
-    }
+  //this is the file that stores the projects
+  private prj_json = require('../../assets/projects/task.json');
+  //get all projects in the json file
+  get(): Promise<Task[]> {
+      return Promise.resolve(this.prj_json.project);
+  }
+    //
+    // get() {
+    //     console.log('message');
+    //     var json = require('../../assets/projects/task.json');
+    //     return json.project;
+    // }
 }
